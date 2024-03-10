03bd2ac31fe433bf1bab04519df61635b3ad5d3c
Andelf <andelf@gmail.com>
2024-03-10 10:50:09 +0800

    Add button click example

--- examples/sdi_print.rs
+++ examples/sdi_print.rs
@@ -13,7 +13,9 @@ fn main() -> ! {
     println!("hello world!");
 
     println!("Flash size: {}kb", hal::signature::flash_size_kb());
-    println!("Chip ID: {:x?}", hal::signature::unique_id());
+    println!("Chip UID: {:x?}", hal::signature::unique_id());
+    let chip_id = hal::signature::chip_id();
+    println!("Chip {}, DevID: 0x{:x}", chip_id.name(), chip_id.dev_id());
 
     loop {
         println!("hello world!");
