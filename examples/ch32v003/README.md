ed4cb7a0b3af738595b11875232a71688408404b
Andelf <andelf@gmail.com>
2024-03-15 11:23:16 +0800

    Add CH32V0 demo

---
+++ examples/ch32v003/README.md
@@ -0,0 +1,7 @@
+# ch32v003-examples
+
+## Boards
+
+### nanoCH32V003
+
+Link: [wuxx/nanoCH32V003](https://github.com/wuxx/nanoCH32V003)
---
+++ examples/ch32v003/src/bin/sdi_print.rs
@@ -0,0 +1,27 @@
+#![no_std]
+#![no_main]
+
+//! SDI debug print
+
+use hal::println;
+use {ch32_hal as hal, panic_halt as _};
+
+#[qingke_rt::entry]
+fn main() -> ! {
+    hal::debug::SDIPrint::enable();
+
+    println!("hello world!");
+
+    println!("Flash size: {}kb", hal::signature::flash_size_kb());
+    println!("Chip UID: {:x?}", hal::signature::unique_id());
+    let chip_id = hal::signature::chip_id();
+    println!("Chip {}, DevID: 0x{:x}", chip_id.name(), chip_id.dev_id());
+
+    loop {
+        println!("hello world!");
+
+        unsafe {
+            qingke::riscv::asm::delay(10_000_000);
+        }
+    }
+}
