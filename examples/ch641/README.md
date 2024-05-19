3f647bd4d2e64c26b7a245f4257b8564b76b7184
Andelf <andelf@gmail.com>
2024-05-20 00:30:31 +0800

    feat: CH641 basic demo

---
+++ examples/ch641/README.md
@@ -0,0 +1,16 @@
+# ch641-examples
+
+## Boards
+
+### CH641P-R0-1v0
+
+Official EVT
+
+### Dino-Board
+
+Dino-Board is a development board for CH641. It is designed to be compatible with the official CH641P-R0-1v0 board.
+
+(link coming soon)
+
+- LED1: PB1
+- LED2: PA6
---
+++ examples/ch641/src/bin/embassy_blinky.rs
@@ -0,0 +1,49 @@
+#![no_std]
+#![no_main]
+#![feature(type_alias_impl_trait)]
+
+use ch32_hal as hal;
+use embassy_executor::Spawner;
+use embassy_time::Timer;
+use hal::gpio::{AnyPin, Level, Output, Pin};
+use hal::println;
+
+#[embassy_executor::task(pool_size = 2)]
+async fn blink(pin: AnyPin, interval_ms: u64) {
+    let mut led = Output::new(pin, Level::Low, Default::default());
+
+    loop {
+        led.set_high();
+        Timer::after_millis(interval_ms).await;
+        led.set_low();
+        Timer::after_millis(interval_ms).await;
+    }
+}
+
+#[embassy_executor::main(entry = "qingke_rt::entry")]
+async fn main(spawner: Spawner) -> ! {
+    hal::debug::SDIPrint::enable();
+    let mut config = hal::Config::default();
+    config.rcc = hal::rcc::Config::SYSCLK_FREQ_48MHZ_HSI;
+    let p = hal::init(config);
+    hal::embassy::init();
+
+    println!("CHIP signature => {}", hal::signature::chip_id().name());
+    println!("Clocks {:?}", hal::rcc::clocks());
+
+    // let mut led = Output::new(p.PC4, Level::Low, Default::default());
+
+    spawner.spawn(blink(p.PB1.degrade(), 110)).unwrap();
+
+    loop {
+        Timer::after_millis(1000).await;
+        println!("tick");
+    }
+}
+
+#[panic_handler]
+fn panic(info: &core::panic::PanicInfo) -> ! {
+    let _ = hal::println!("\n\n\n{}", info);
+
+    loop {}
+}
