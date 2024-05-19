7dd97339661d3bfe54cc8adf275ec76215bc7f6b
Andelf <andelf@gmail.com>
2024-05-20 00:30:31 +0800

    feat: CH641 basic demo

---
+++ examples/ch641/src/bin/blinky.rs
@@ -0,0 +1,27 @@
+#![no_std]
+#![no_main]
+#![feature(type_alias_impl_trait)]
+
+use hal::delay::Delay;
+use hal::gpio::{Level, Output};
+use {ch32_hal as hal, panic_halt as _};
+
+#[qingke_rt::entry]
+fn main() -> ! {
+    hal::debug::SDIPrint::enable();
+    //    let mut config = hal::Config::default();
+    //  config.rcc = hal::rcc::Config::SYSCLK_FREQ_48MHZ_HSE;
+    let p = hal::init(Default::default());
+
+    let mut delay = Delay;
+
+    let mut led = Output::new(p.PB1, Level::Low, Default::default());
+    loop {
+        led.toggle();
+
+        delay.delay_ms(500);
+        hal::println!("toggle!");
+        let val = hal::pac::SYSTICK.cnt().read();
+        hal::println!("systick: {}", val);
+    }
+}
