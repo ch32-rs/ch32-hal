2454e26e6b8918ee23d443b67f07d9dae01238bc
Romain Reignier <romain@reignier.sh>
2024-06-18 13:36:54 +0200

    demo: add a simple v307 blinky example and rename the original embassy_blinky

--- examples/ch32v307/src/bin/blinky.rs
+++ examples/ch32v307/src/bin/blinky.rs
@@ -2,33 +2,19 @@
 #![no_main]
 #![feature(type_alias_impl_trait)]
 
-use embassy_executor::Spawner;
-use embassy_time::{Duration, Timer};
-use hal::gpio::{AnyPin, Level, Output, Pin};
+use hal::delay::Delay;
+use hal::gpio::{Level, Output};
 use {ch32_hal as hal, panic_halt as _};
 
-#[embassy_executor::task(pool_size = 3)]
-async fn blink(pin: AnyPin, interval_ms: u64) {
-    let mut led = Output::new(pin, Level::Low, Default::default());
-
-    loop {
-        led.set_high();
-        Timer::after(Duration::from_millis(interval_ms)).await;
-        led.set_low();
-        Timer::after(Duration::from_millis(interval_ms)).await;
-    }
-}
-
-#[embassy_executor::main(entry = "qingke_rt::entry")]
-async fn main(spawner: Spawner) -> ! {
+#[qingke_rt::entry]
+fn main() -> ! {
     let p = hal::init(Default::default());
-    hal::embassy::init();
 
-    // GPIO
-    spawner.spawn(blink(p.PA15.degrade(), 1000)).unwrap();
-    spawner.spawn(blink(p.PB4.degrade(), 100)).unwrap();
-    // spawner.spawn(blink(p.PB8.degrade(), 100)).unwrap();
+    let mut led = Output::new(p.PA15, Level::Low, Default::default());
+    let mut delay = Delay;
+
     loop {
-        Timer::after_millis(2000).await;
+        led.toggle();
+        delay.delay_ms(1000);
     }
 }
