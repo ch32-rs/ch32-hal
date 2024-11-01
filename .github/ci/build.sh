91ceba25e80ab87d96487f6d34f2c4bfbf0b89c7
Andelf <andelf@gmail.com>
2024-11-01 16:23:56 +0800

    fix(ci): disable non-embassy build temporarily

--- .github/ci/build.sh
+++ .github/ci/build.sh
@@ -7,7 +7,7 @@ set -euo pipefail
 cargo build --features ${CHIP},embassy,rt,memory-x --no-default-features --target ${TARGET}
 
 # Build without embassy
-cargo build --features ${CHIP},rt --no-default-features --target ${TARGET}
+# cargo build --features ${CHIP},rt --no-default-features --target ${TARGET}
 
 # Build without embassy and rt
-cargo build --features ${CHIP} --no-default-features --target ${TARGET}
+# cargo build --features ${CHIP} --no-default-features --target ${TARGET}
