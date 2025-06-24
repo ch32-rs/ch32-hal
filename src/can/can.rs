e1c3c2251f81d9417dfac024a96f63e98a28d581
Micke Prag <micke@orbital-systems.com>
2025-06-24 23:17:57 +0200

    Split the implementation into different modes
    Move the current implementation to the nb mode

--- src/can/can.rs
+++ src/can/can.rs
@@ -1,14 +1,18 @@
+use core::marker::PhantomData;
+
 use super::enums::*;
 use super::filter::{BitMode, FilterMode};
 use super::{CanFilter, CanFrame};
 use crate::can::registers::Registers;
 use crate::can::util;
+use crate::mode::{Mode, NonBlocking};
 use crate::{into_ref, pac, peripherals, Peripheral, PeripheralRef, RccPeripheral, RemapPeripheral};
 
-pub struct Can<'d, T: Instance> {
+pub struct Can<'d, T: Instance, M: Mode> {
     _peri: PeripheralRef<'d, T>,
     fifo: CanFifo,
     last_mailbox_used: usize,
+    _phantom: PhantomData<(&'d mut T, M)>,
 }
 
 #[derive(Debug)]
@@ -16,11 +20,80 @@ pub enum CanInitError {
     InvalidTimings,
 }
 
-impl<'d, T: Instance> Can<'d, T> {
+impl<'d, T: Instance> Can<'d, T, NonBlocking> {
+    pub fn new_nb<const REMAP: u8>(
+        peri: impl Peripheral<P = T> + 'd,
+        rx: impl Peripheral<P = impl RxPin<T, REMAP>> + 'd,
+        tx: impl Peripheral<P = impl TxPin<T, REMAP>> + 'd,
+        fifo: CanFifo,
+        mode: CanMode,
+        bitrate: u32,
+    ) -> Result<Self, CanInitError> {
+        Self::new_inner(peri, rx, tx, fifo, mode, bitrate)
+    }
+
+    /// Puts a frame in the transmit buffer to be sent on the bus.
+    ///
+    /// If the transmit buffer is full, this function will try to replace a pending
+    /// lower priority frame and return the frame that was replaced.
+    /// Returns `Err(WouldBlock)` if the transmit buffer is full and no frame can be
+    /// replaced.
+    pub fn transmit(&mut self, frame: &CanFrame) -> nb::Result<Option<CanFrame>, CanError> {
+        let mailbox_num = match Registers::new::<T>().find_free_mailbox() {
+            Some(n) => n,
+            None => return Err(nb::Error::WouldBlock),
+        };
+
+        Registers::new::<T>().write_frame_mailbox(mailbox_num, frame);
+        self.last_mailbox_used = mailbox_num;
+
+        // Success in readying packet for transmit. No packets can be replaced in the
+        // transmit buffer so return None in accordance with embedded-can.
+        Ok(None)
+    }
+
+    /// Try to read the next message from the queue.
+    /// If there are no messages, an error is returned.
+    pub fn try_recv(&self) -> nb::Result<CanFrame, CanError> {
+        let can = &T::regs();
+        let fifo = self.fifo.val();
+
+        //check pending messages
+        if can.rfifo(self.fifo.val()).read().fmp() == 0 {
+            return Err(nb::Error::WouldBlock);
+        }
+
+        let dlc = can.rxmdtr(fifo).read().dlc() as usize;
+        if dlc > 8 {
+            return Err(nb::Error::Other(CanError::Form));
+        }
+        let rxmir = can.rxmir(fifo).read();
+
+        let id = if rxmir.ide() {
+            let raw_id = ((rxmir.stid() as u32) << 18) | rxmir.exid();
+            embedded_can::Id::from(unsafe { embedded_can::ExtendedId::new_unchecked(raw_id & 0x1FFFFFFF) })
+        } else {
+            embedded_can::Id::Standard(embedded_can::StandardId::new(rxmir.stid()).unwrap())
+        };
+
+        let frame_data_unordered: u64 = ((can.rxmdhr(fifo).read().0 as u64) << 32) | can.rxmdlr(fifo).read().0 as u64;
+
+        let frame = CanFrame::new_from_data_registers(id, frame_data_unordered, dlc);
+
+        can.rfifo(fifo).write(|w| {
+            //set the data was read
+            w.set_rfom(true);
+        });
+
+        Ok(frame)
+    }
+}
+
+impl<'d, T: Instance, M: Mode> Can<'d, T, M> {
     /// Assumes AFIO & PORTB clocks have been enabled by HAL.
     ///
     /// CAN_RX is mapped to PB8, and CAN_TX is mapped to PB9.
-    pub fn new<const REMAP: u8>(
+    fn new_inner<const REMAP: u8>(
         peri: impl Peripheral<P = T> + 'd,
         rx: impl Peripheral<P = impl RxPin<T, REMAP>> + 'd,
         tx: impl Peripheral<P = impl TxPin<T, REMAP>> + 'd,
@@ -34,6 +107,7 @@ impl<'d, T: Instance> Can<'d, T> {
             _peri: peri,
             fifo,
             last_mailbox_used: usize::MAX,
+            _phantom: PhantomData,
         };
         T::enable_and_reset(); // Enable CAN peripheral
 
@@ -91,25 +165,6 @@ impl<'d, T: Instance> Can<'d, T> {
         can.fctlr().modify(|w| w.set_finit(false)); // Exit filter init mode
     }
 
-    /// Puts a frame in the transmit buffer to be sent on the bus.
-    ///
-    /// If the transmit buffer is full, this function will try to replace a pending
-    /// lower priority frame and return the frame that was replaced.
-    /// Returns `Err(WouldBlock)` if the transmit buffer is full and no frame can be
-    /// replaced.
-    pub fn transmit(&self, frame: &CanFrame) -> nb::Result<Option<CanFrame>, CanError> {
-        let mailbox_num = match Registers::new::<T>().find_free_mailbox() {
-            Some(n) => n,
-            None => return Err(nb::Error::WouldBlock),
-        };
-
-        Registers::new::<T>().write_frame_mailbox(mailbox_num, frame);
-
-        // Success in readying packet for transmit. No packets can be replaced in the
-        // transmit buffer so return None in accordance with embedded-can.
-        Ok(None)
-    }
-
     /// Retrieves status of the last frame transmission
     pub fn transmit_status(&self) -> TxStatus {
         if self.last_mailbox_used > 2 {
@@ -118,44 +173,11 @@ impl<'d, T: Instance> Can<'d, T> {
 
         Registers::new::<T>().transmit_status(self.last_mailbox_used)
     }
-
-    /// Try to read the next message from the queue.
-    /// If there are no messages, an error is returned.
-    pub fn try_recv(&self) -> nb::Result<CanFrame, CanError> {
-        let can = &T::regs();
-        let fifo = self.fifo.val();
-
-        //check pending messages
-        if can.rfifo(self.fifo.val()).read().fmp() == 0 {
-            return Err(nb::Error::WouldBlock);
-        }
-
-        let dlc = can.rxmdtr(fifo).read().dlc() as usize;
-        let rxmir = can.rxmir(fifo).read();
-
-        let id = if rxmir.ide() {
-            let raw_id = ((rxmir.stid() as u32) << 18) | rxmir.exid();
-            embedded_can::Id::from(unsafe { embedded_can::ExtendedId::new_unchecked(raw_id & 0x1FFFFFFF) })
-        } else {
-            embedded_can::Id::Standard(embedded_can::StandardId::new(rxmir.stid()).unwrap())
-        };
-
-        let frame_data_unordered: u64 = ((can.rxmdhr(fifo).read().0 as u64) << 32) | can.rxmdlr(fifo).read().0 as u64;
-
-        let frame = CanFrame::new_from_data_registers(id, frame_data_unordered, dlc);
-
-        can.rfifo(fifo).write(|w| {
-            //set the data was read
-            w.set_rfom(true);
-        });
-
-        Ok(frame)
-    }
 }
 
 /// These trait methods are only usable within the embedded_can context.
 /// Under normal use of the [Can] instance,
-impl<'d, T> embedded_can::nb::Can for Can<'d, T>
+impl<'d, T> embedded_can::nb::Can for Can<'d, T, NonBlocking>
 where
     T: Instance,
 {
