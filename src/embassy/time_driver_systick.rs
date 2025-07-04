aa21f5e15409f2f222ed4b68cbede787a27f14be
Marcel Maage <git@maage.dev>
2025-07-04 23:03:39 +0200

    embassy tick back to default. Applied patch for systick

--- src/embassy/time_driver_systick.rs
+++ src/embassy/time_driver_systick.rs
@@ -1,6 +1,6 @@
 //! SysTick-based time driver.
 
-use core::cell::{Cell, RefCell};
+use core::cell::RefCell;
 use core::sync::atomic::{AtomicU32, Ordering};
 use critical_section::CriticalSection;
 use embassy_sync::blocking_mutex::raw::CriticalSectionRawMutex;
@@ -18,30 +18,13 @@ fn SysTick() {
     DRIVER.on_interrupt();
 }
 
-struct AlarmState {
-    timestamp: Cell<u64>,
-}
-
-unsafe impl Send for AlarmState {}
-
-impl AlarmState {
-    const fn new() -> Self {
-        Self {
-            timestamp: Cell::new(u64::MAX),
-        }
-    }
-}
-
 pub struct SystickDriver {
-    period: AtomicU32,
-    /// Timestamp at which to fire alarm. u64::MAX if no alarm is scheduled.
-    alarm: Mutex<CriticalSectionRawMutex, AlarmState>,
+    cnt_per_tick: AtomicU32,
     queue: Mutex<CriticalSectionRawMutex, RefCell<Queue>>,
 }
 
 embassy_time_driver::time_driver_impl!(static DRIVER: SystickDriver = SystickDriver {
-    period: AtomicU32::new(1), // avoid div by zero
-    alarm: Mutex::const_new(CriticalSectionRawMutex::new(), AlarmState::new()),
+    cnt_per_tick: AtomicU32::new(1), // avoid div by zero
     queue: Mutex::new(RefCell::new(Queue::new()))
 });
 
@@ -53,44 +36,37 @@ impl SystickDriver {
         let cnt_per_second = hclk / 8; // HCLK/8
         let cnt_per_tick = cnt_per_second / embassy_time_driver::TICK_HZ;
 
-        self.period.store(cnt_per_tick as u32, Ordering::Relaxed);
+        self.cnt_per_tick.store(cnt_per_tick as u32, Ordering::Relaxed);
 
-        // UNDOCUMENTED:  Avoid initial interrupt
-        r.cmp().write(|w| *w = u64::MAX - 1);
-        r.cmp().write_value(0);
+        r.ctlr().write(|w| {
+            // Everything else is set to default (0)
+            w.set_init(true); // Initialize counter
+            w.set_ste(true); // Enable counter
+        });
 
-        //Count value compare flag
+        // Write 0 to both halves of the compare register
+        r.cmph().write_value(0);
+        r.cmpl().write_value(0);
+
+        // Count value compare flag
         r.sr().write(|w| w.set_cntif(false)); // clear
 
-        // Configration: Upcount, No reload, HCLK as clock source
+        // Configration: Upcount, No reload, HCLK/8 as clock source
         r.ctlr().modify(|w| {
-            //  w.set_init(true);
-            w.set_mode(vals::Mode::UPCOUNT);//Counter mode
-            w.set_stre(false);//Auto reload count enable bit
-            w.set_stclk(vals::Stclk::HCLK_DIV8);//Counter system clock sourse selection bit
-            w.set_ste(true);//Counter enable control bit
+            w.set_mode(vals::Mode::UPCOUNT); // Counter mode
+            w.set_stre(false); // Auto reload count enable bit
+            w.set_stclk(vals::Stclk::HCLK_DIV8); // Counter system clock sourse selection bit
         });
     }
 
-    #[inline(always)]
     fn on_interrupt(&self) {
         let r = &crate::pac::SYSTICK;
-        //Count value compare flag
+        // Count value compare flag
         r.sr().write(|w| w.set_cntif(false)); // clear IF
 
-        let period = self.period.load(Ordering::Relaxed) as u64;
-
-        let next_timestamp = critical_section::with(|cs| {
-            let next = self.alarm.borrow(cs).timestamp.get();
-            if next > self.now() + 1 {
-                return next;
-            }
+        critical_section::with(|cs| {
             self.trigger_alarm(cs);
-            return u64::MAX;
         });
-
-        let new_cmp = u64::min(next_timestamp.saturating_mul(period), self.raw_cnt().wrapping_add(period));
-        r.cmp().write_value(new_cmp.saturating_add(1));
     }
 
     #[inline]
@@ -100,32 +76,35 @@ impl SystickDriver {
     }
 
     fn trigger_alarm(&self, cs: CriticalSection) {
-        let mut next = self.queue.borrow(cs).borrow_mut().next_expiration(self.now());
+        let mut next = self.queue.borrow(cs).borrow_mut().next_expiration(self.raw_cnt());
         while !self.set_alarm(cs, next) {
-            next = self.queue.borrow(cs).borrow_mut().next_expiration(self.now());
+            next = self.queue.borrow(cs).borrow_mut().next_expiration(self.raw_cnt());
         }
     }
 
-    fn set_alarm(&self, cs: CriticalSection, timestamp: u64) -> bool {
+    fn set_alarm(&self, _cs: CriticalSection, next_alarm_cnt: u64) -> bool {
         let r = &crate::pac::SYSTICK;
 
-        self.alarm.borrow(cs).timestamp.set(timestamp);
-
-        let period = self.period.load(Ordering::Relaxed) as u64;
-
-        // See-also: https://github.com/ch32-rs/ch32-hal/issues/4
-        let t = self.raw_cnt();
-        let timestamp = timestamp.saturating_mul(period);
-        if timestamp <= t {
+        // TODO move this to schedule_wake
+        if next_alarm_cnt <= self.raw_cnt() {
             // If alarm timestamp has passed the alarm will not fire.
             // Disarm the alarm and return `false` to indicate that.
-
-            self.alarm.borrow(cs).timestamp.set(u64::MAX);
-
             return false;
         }
-        //Counter interrupt enable control bit
+
+        // Counter interrupt enable control bit
+        r.cmph().write_value((next_alarm_cnt >> 32) as u32);
+        r.cmpl().write_value((next_alarm_cnt) as u32);
         r.ctlr().modify(|w| w.set_stie(true));
+        r.sr().write(|w| w.set_cntif(false));
+
+        if next_alarm_cnt <= self.raw_cnt() {
+            // If alarm timestamp has passed the alarm will not fire.
+            // Disarm the alarm and return `false` to indicate that.
+            r.ctlr().modify(|w| w.set_stie(false));
+            r.sr().write(|w| w.set_cntif(false));
+            return false;
+        }
 
         true
     }
@@ -133,18 +112,19 @@ impl SystickDriver {
 
 impl Driver for SystickDriver {
     fn now(&self) -> u64 {
-        let period = self.period.load(Ordering::Relaxed) as u64;
-        self.raw_cnt() / period
+        let cnt_per_tick = self.cnt_per_tick.load(Ordering::Relaxed) as u64;
+        self.raw_cnt() / cnt_per_tick
     }
 
-    fn schedule_wake(&self, at: u64, waker: &core::task::Waker) {
+    fn schedule_wake(&self, ticks: u64, waker: &core::task::Waker) {
+        let cnt_per_tick = self.cnt_per_tick.load(Ordering::Relaxed) as u64;
         critical_section::with(|cs| {
             let mut queue = self.queue.borrow(cs).borrow_mut();
 
-            if queue.schedule_wake(at, waker) {
-                let mut next = queue.next_expiration(self.now());
+            if queue.schedule_wake(ticks * cnt_per_tick, waker) {
+                let mut next = queue.next_expiration(self.raw_cnt());
                 while !self.set_alarm(cs, next) {
-                    next = queue.next_expiration(self.now());
+                    next = queue.next_expiration(self.raw_cnt());
                 }
             }
         })
