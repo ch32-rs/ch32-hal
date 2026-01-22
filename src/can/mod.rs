ab4bbdb5bfccb4ceadddc159ceb57275deb587a4
Micke Prag <micke.prag@gmail.com>
2026-01-22 21:37:19 +0100

    feat(can): Implement async transmit

--- src/can/mod.rs
+++ src/can/mod.rs
@@ -5,7 +5,7 @@ mod frame;
 mod registers;
 mod util;
 
-pub use can::{Can, Instance, TxPin, RxPin, ReceiveInterruptHandler, Config};
+pub use can::{Can, Instance, TxPin, RxPin, ReceiveInterruptHandler, TransmitInterruptHandler, Config};
 pub use embedded_can::{ExtendedId, Id, StandardId};
 pub use enums::{CanError, CanFifo, CanMode, TxStatus};
 pub use filter::{Bit16Mode, Bit32Mode, CanFilter, ListMode, MaskMode};
