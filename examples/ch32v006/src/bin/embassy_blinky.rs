08af1e7bc025f39f31f637cc98d3529708031c03
Andrey Lebedev <andreyelplus@gmail.com>
2026-02-26 11:37:01 +0300

    Add CH32V00X examples

---
+++ examples/ch32v006/src/bin/embassy_blinky.rs
@@ -0,0 +1,49 @@
+#![no_std]
+#![no_main]
+#![feature(type_alias_impl_trait)]
+#![feature(impl_trait_in_assoc_type)]
+
+use ch32_hal as hal;
+use embassy_executor::Spawner;
+use embassy_time::Timer;
+use hal::gpio::{AnyPin, Level, Output};
+use hal::{println, Peri};
+
+#[embassy_executor::task(pool_size = 2)]
+async fn blink(pin: Peri<'static, AnyPin>, interval_ms: u64) {
+    let mut led = Output::new(pin, Level::Low, Default::default());
+
+    loop {
+        led.set_high();
+        Timer::after_millis(interval_ms).await;
+        led.set_low();
+        Timer::after_millis(interval_ms).await;
+    }
+}
+
+#[embassy_executor::main(entry = "qingke_rt::entry")]
+async fn main(spawner: Spawner) -> ! {
+    hal::debug::SDIPrint::enable();
+
+    let mut config = hal::Config::default();
+    config.rcc = hal::rcc::Config::SYSCLK_FREQ_48MHZ_HSI;
+    let p = hal::init(config);
+
+    println!("CHIP signature => {}", hal::signature::chip_id().name());
+    println!("Clocks {:?}", hal::rcc::clocks());
+
+    spawner.spawn(blink(p.PD6.into(), 110)).unwrap();
+    spawner.spawn(blink(p.PA4.into(), 270)).unwrap();
+
+    loop {
+        Timer::after_millis(1000).await;
+        println!("tick");
+    }
+}
+
+#[panic_handler]
+fn panic(info: &core::panic::PanicInfo) -> ! {
+    let _ = hal::println!("\n\n\n{}", info);
+
+    loop {}
+}
---
+++ examples/ch32v006/src/bin/sdi_print.rs
@@ -0,0 +1,26 @@
+#![no_std]
+#![no_main]
+
+//! SDI debug print
+
+use hal::println;
+use {ch32_hal as hal, panic_halt as _};
+
+#[qingke_rt::entry]
+fn main() -> ! {
+    hal::debug::SDIPrint::enable();
+
+    println!("Start");
+
+    println!("Flash size: {}kb", hal::signature::flash_size_kb());
+    println!("Chip UID: {:x?}", hal::signature::unique_id());
+    let chip_id = hal::signature::chip_id();
+    println!("Chip {}, DevID: 0x{:x}", chip_id.name(), chip_id.dev_id());
+    println!("Clocks: {:?}", hal::rcc::clocks());
+
+    loop {
+        println!("hello world!");
+
+        qingke::riscv::asm::delay(10_000_000);
+    }
+}
